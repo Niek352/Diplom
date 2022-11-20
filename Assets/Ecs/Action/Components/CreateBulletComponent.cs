93c2c2a281d13dc8b3a049a55b3c5bac93706561
Pavel <oavelasdqwe8@gmail.com>
2022-11-20 17:21:29 +0300

    add new

---
+++ Assets/Ecs/Action/Components/CreateBulletComponent.cs
@@ -0,0 +1,13 @@
+﻿using JCMG.EntitasRedux;
+using UnityEngine;
+
+namespace Ecs.Action.Components
+{
+	[Action]
+	public class CreateBulletComponent : IComponent
+	{
+		public Vector3 Velocity;
+		public Vector3 SpawnPoint;
+		public float LifeTime;
+	}
+}
